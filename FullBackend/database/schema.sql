a9f0e1a317ff18cc69d7040aa0f98b118e981ef0
M3llyz <michaelageym@gmail.com>
2026-04-09 21:03:29 +0100

    add database schema for the dropBox

---
+++ FullBackend/database/schema.sql
@@ -0,0 +1,53 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema file_dropbox
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema file_dropbox
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `file_dropbox` DEFAULT CHARACTER SET utf8 ;
+USE `file_dropbox` ;
+
+-- -----------------------------------------------------
+-- Table `file_dropbox`.`Users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `file_dropbox`.`Users` (
+  `UserID` INT NOT NULL AUTO_INCREMENT,
+  `Username` VARCHAR(50) NOT NULL,
+  `PasswordHash` VARCHAR(255) NOT NULL,
+  `CreatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`UserID`),
+UNIQUE INDEX `Username_UNIQUE` (`Username` ASC) VISIBLE
+)ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `file_dropbox`.`Files`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `file_dropbox`.`Files` (
+  `FileID` INT NOT NULL AUTO_INCREMENT,
+  `UserID` INT NOT NULL,
+  `StoredName` VARCHAR(255) NOT NULL,
+  `OrigName` VARCHAR(255) NOT NULL,
+  `UploadedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`FileID`),
+  INDEX `UserID_idx` (`UserID` ASC) VISIBLE,
+  CONSTRAINT `fk_Files_Users`
+    FOREIGN KEY (`UserID`)
+    REFERENCES `file_dropbox`.`Users` (`UserID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
