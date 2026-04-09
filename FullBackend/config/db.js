c868765b237acce8f33fc10c32a0dcde95612f7f
Tagg02 <harrison.taggart@hotmail.com>
2026-04-09 22:35:05 +0100

    Use MYSQL_URL connection string for Railway DB

--- FullBackend/config/db.js
+++ FullBackend/config/db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
+const pool = mysql.createPool(process.env.MYSQL_URL || {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
